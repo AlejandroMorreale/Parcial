b1f85a5b9fd75cf18fabc8d574ec8a94e6e4cfea
AlejandroMorreale <alee.eme@gmail.com>
2016-10-03 20:38:32 -0300

    Add files via upload

--- nexoAdministrador.php
+++ nexoAdministrador.php
@@ -1,3 +1,15 @@
 <?php
 
+class nexoAdministrador
+{
+	public static function Guardar($patente)
+	{
+		$archivo=fopen("usuarioTxt/usuariosLogin.txt", "a");//escribe y mantiene la informacion existente		
+		$ahora="Prueba";//date("Y-m-d H:i:s"); 
+		$renglon="$patente"."=>".$ahora."\n";
+		fwrite($archivo, $renglon); 		 
+		fclose($archivo);
+	}
+}
+
 ?>
